e2d151e635614f5fabaa8651a0fac42f8fca7a0c
Federico Santoro <federicogabsantoro@gmail.com>
2022-11-04 15:28:15 -0300

    practica

--- ejercicio4/ejercicio4.js
+++ ejercicio4/ejercicio4.js
@@ -34,9 +34,43 @@ const Pizzas = [
     id: 6,
     ingredientes: ["Queso", "Salsa", "tomate", "ajo", "Jamon", "Piña"],
     precio: "900",
-  }
+  },
 ];
 
+//Punto a
+console.log("Pizzas con ID Impar: ");
+Pizzas.forEach((element) => {
+  if (element.id % 2 != 0) console.log(element.nombre);
+});
+
+//Punto b
+console.log(" ");
+console.log("Hay pizzas a menos de $600? ");
+if (Pizzas.some((element) => element.precio < "600")) {
+  console.log("Si, hay pizzas a menos de $600");
+} else {
+  console.log("No, no hay pizzas a menos de $600");
+}
+
+//Punto c
+console.log(" ");
+console.log("Las pizzas y sus precios son: ");
+Pizzas.forEach((element) =>
+  console.log(
+    "La Pizza " + element.nombre + " tiene precio de $" + element.precio
+  )
+);
+
+//Punto d
+console.log(" ");
+console.log("Las pizzas y sus ingredientes son: ");
+Pizzas.forEach((element) => {
+  console.log(" ");
+  console.log("La pizza " + element.nombre + " tiene de ingredientes: ");
+  element.ingredientes.forEach((element2) => console.log(element2));
+});
+
+/*
 //Punto a
 console.log(" ");
 console.log("Pizzas ID Impar: ");
@@ -49,7 +83,7 @@ for (let i = 0; i < Pizzas.length; i++) {
 //Punto b
 console.log(" ");
 let sino = false;
-console.log("Pizzas a menos de $600: ");
+console.log(" Hay pizzas a menos de $600? ");
 for (let i = 0; i < Pizzas.length; i++) {
   if (Pizzas[i].precio < 600) {
     sino = true;
@@ -57,6 +91,9 @@ for (let i = 0; i < Pizzas.length; i++) {
 }
 if (sino == true) {
   console.log("Si, hay pizzas a menos de $600");
+}
+else {
+  console.log("No, no hay pizzas a menos de $600");
 }
 
 //Punto c
@@ -78,3 +115,4 @@ for (let i = 0; i < Pizzas.length; i++) {
   }
   console.log(" ");
 }
+*/
